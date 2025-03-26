f456c59f483f929030fc1b7661ae5b2f07beef0c
axlet <ivannikov495@gmail.com>
2025-03-26 21:30:06 +0700

    init: settings for docker and postgres

---
+++ src/main/java/com/example/_test/TestController.java
@@ -0,0 +1,12 @@
+package com.example._test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Hello World";
+    }
+}
